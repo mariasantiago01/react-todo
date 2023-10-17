07a9af6129e05d0228167838c4fc4c7e9add1ab6
Maria S <mariasantiago0109@gmail.com>
2023-10-17 18:58:07 -0400

    completed lesson 1.3

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
-function AddTodoForm () {
+const AddTodoForm  = (props) => {
+    
+    const handleAddTodo = (event) => {
+        event.preventDefault();
+        let todoTitle = event.target.elements.title.value;
+        console.log(todoTitle);
+        props.onAddTodo(todoTitle);
+        event.target.reset();
+    };
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleAddTodo}>
                 <label htmlFor='todoTitle'>Title</label>
-                <input id='todoTitle'></input>
-                <button>Add</button>
+                <input id='todoTitle' name='title'></input>
+                <button type='submit'>Add</button>
             </form>
         </div>
     );
--- src/App.js
+++ src/App.js
@@ -2,14 +2,37 @@ import React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-function App() {
+const App = () =>{
+  const todoList = [
+    {
+      id: 1,
+      title: "finish current book",
+    },
+    {
+      id: 2,
+      title: "complete homework",
+    },
+    {
+      id: 3,
+      title: "plan out week ahead",
+    },
+    {
+      id: 4,
+      title: "do laundry",
+    },
+  ];
+
+  const [newTodo, setNewTodo] = React.useState('');
+
   return (
     <div>
       <h1>Todo List</h1>
       
-      <AddTodoForm />
+      <AddTodoForm onAddTodo={setNewTodo}/>
       
-      < TodoList />
+      <p>{newTodo}</p>
+
+      <TodoList todo={todoList}/>
     </div>
   );
 }
