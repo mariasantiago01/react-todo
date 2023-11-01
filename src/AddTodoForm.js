b6c2fed3cd09b9430b4817777e1d8baa6510db02
Maria S <mariasantiago0109@gmail.com>
2023-11-01 19:47:52 -0400

    control 'add todo' input

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const AddTodoForm  = (props) => {
+    const [todoTitle, setTodoTitle] = React.useState('');
     
+    const handleTitleChange = (event) => {
+        const newTodoTitle = event.target.value;
+        // console.log(newTodoTitle);
+        setTodoTitle(newTodoTitle);
+    };
+
     const handleAddTodo = (event) => {
         event.preventDefault();
-        let todoTitle = event.target.elements.title.value;
+        // let todoTitle = event.target.elements.title.value;
         console.log(todoTitle);
         props.onAddTodo(todoTitle);
         event.target.reset();
@@ -14,7 +21,12 @@ const AddTodoForm  = (props) => {
         <div>
             <form onSubmit={handleAddTodo}>
                 <label htmlFor='todoTitle'>Title</label>
-                <input id='todoTitle' name='title'></input>
+                <input 
+                id='todoTitle' 
+                name='title'
+                value={props.todoTitle}
+                onChange={handleTitleChange}
+                ></input>
                 <button type='submit'>Add</button>
             </form>
         </div>
--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
+import todoTitle from './AddTodoForm'
 
 // This is the main app component. The root component.
 const App = () =>{
   // our to do list array
-  // const todoList = [
-  //   {
-  //     id: 1,
-  //     title: "finish current book",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "complete homework",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "plan out week ahead",
-  //   },
-  //   {
-  //     id: 4,
-  //     title: "do laundry",
-  //   },
-  // ];
+  // const theTodoList = [
+  // //   {
+  // //     id: 1,
+  // //     title: "finish current book",
+  // //   },
+  // //   {
+  // //     id: 2,
+  // //     title: "complete homework",
+  // //   },
+  // //   {
+  // //     id: 3,
+  // //     title: "plan out week ahead",
+  // //   },
+  // //   {
+  // //     id: 4,
+  // //     title: "do laundry",
+  // //   },
+  // // ];
   
   const [newTodo, setNewTodo] = React.useState('');
-
   const [todoList, setTodoList] = React.useState([]);
-
-
 
   return (
     <div>
