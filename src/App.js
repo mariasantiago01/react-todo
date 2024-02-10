d8116e64c3a1b2d5030e31bc0d4749490c725d44
Maria S <mariasantiago0109@gmail.com>
2024-02-09 21:44:37 -0500

    Implemented a nav bar with corresponding CSS.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,35 @@ import React from 'react';
 import TodoContainer from './components/TodoContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import style from './App.module.css';
-import { FaTasks } from "react-icons/fa";
+import { FaTasks, FaHome } from "react-icons/fa";
 
-// This is the main app component. The root component.
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element= {
+        <Route path='/' element={
+            <>
+              <nav className={style.Nav}>
+                <h1 className={style.TodoTitle}><FaTasks className={style.TodoLogo}/>  Todo List</h1>
+              </nav>
+              <div>
+                <div>
+                  <a href='/todoList'>My Current Todo List</a>
+                </div>
+              </div>
+            </>
+          }/>
+        <Route path="/todoList" element= {
           <>
-            <h1 className={style.TodoTitle}><FaTasks className={style.TodoLogo}/>Todo List</h1>
+            <nav className={style.Nav}>
+              <h1 className={style.TodoTitle}><FaTasks className={style.TodoLogo}/>  Todo List</h1>
+              <button id='editButton'className={style.HomeButton}> <a href='/'><FaHome className={style.HomeLogo}/> </a> </button>
+            </nav>
+            <span className={style.Path}> <a href='/' className={style.Link}>Home</a>/<a href='/todoList' className={style.Link}>Todo Table 1</a></span>
+
             <TodoContainer/>
           </>
           }/> 
-        <Route path='/new' element={
-          <>
-            <h1>New Todo List</h1>
-          </>
-        }/>
       </Routes>
     </BrowserRouter>
   );
